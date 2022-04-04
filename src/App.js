22e5a9f1cae99d8b63f54d4680a6327a7be83141
Rômulo Gomes <romulo.gomes.mendes@hotmail.com>
2022-04-04 18:27:52 -0300

    adicionada paginas do projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>cost</h1>
-    </div>
+    <Router> 
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/Company">Empresa</Link>
+        <Link to="/Contact">Contato</Link>
+        <Link to="/NewProject">Novo Projeto</Link>
+      </ul>
+
+          <Routes>
+            <Route path= "/" element={<Home/>}/> 
+            <Route path= "/Company" element={<Company/>}/> 
+            <Route path= "/Contact" element={<Contact/>}/> 
+            <Route path= "/NewProject" element={<NewProject/>}/>    
+          </Routes>
+
+      <p>Footer</p>
+
+    </Router>
   );
 }
 
