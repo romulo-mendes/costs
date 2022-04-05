094cb6b8dd4ebd261e05e262dff4c1d2d93ba130
Rômulo Gomes <romulo.gomes.mendes@hotmail.com>
2022-04-05 01:53:50 -0300

    fazendo navbar e footer

--- src/App.js
+++ src/App.js
@@ -4,24 +4,22 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+
 
 function App() {
   return (
     <Router> 
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/Company">Empresa</Link>
-        <Link to="/Contact">Contato</Link>
-        <Link to="/NewProject">Novo Projeto</Link>
-      </ul>
-
+      <Navbar />
+        <Container customClass="min-height">
           <Routes>
             <Route path= "/" element={<Home/>}/> 
             <Route path= "/Company" element={<Company/>}/> 
             <Route path= "/Contact" element={<Contact/>}/> 
             <Route path= "/NewProject" element={<NewProject/>}/>    
           </Routes>
-
+        </Container>
       <p>Footer</p>
 
     </Router>
